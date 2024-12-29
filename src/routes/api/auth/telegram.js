822093c4d65ff89fe3131e05e82a66150a87d37b
Artem <iriqtu@gmail.com>
2024-12-30 01:23:29 +0300

    update auth

--- src/routes/api/auth/telegram.js
+++ src/routes/api/auth/telegram.js
@@ -1,9 +1,17 @@
 import express from "express";
 import crypto from "crypto";
 const router = express.Router();
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 router.get("/", (req, res) => {
+  if (!BOT_TOKEN) {
+    throw new Error("BOT_TOKEN is not defined in environment variables");
+  }
   const { hash, ...data } = req.query;
   const secret = crypto.createHash("sha256").update(BOT_TOKEN).digest();
   const checkString = Object.keys(data)
@@ -20,8 +28,15 @@ router.get("/", (req, res) => {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  // Пользователь прошел авторизацию
-  res.json({ success: true, user: data });
+  const token = jwt.sign({ user: data }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  const userInfo = encodeURIComponent(JSON.stringify(data));
+
+  res.redirect(
+    `${process.env.CLIENT_URL}/auth-success?token=${token}&user=${userInfo}`
+  );
 });
 
 export default router;
