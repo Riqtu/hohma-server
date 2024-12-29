e248199ef6596fab36c41480e183801fc4b25dca
Artem <iriqtu@gmail.com>
2024-12-29 20:32:59 +0300

    test

--- src/routes/api/auth/telegram.js
+++ src/routes/api/auth/telegram.js
@@ -3,48 +3,25 @@ import crypto from "crypto";
 const router = express.Router();
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
-const checkTelegramAuth = (data) => {
-  const { hash, ...rest } = data;
-
-  const sortedData = Object.keys(rest)
+app.get("/", (req, res) => {
+  const { hash, ...data } = req.query;
+  const secret = crypto.createHash("sha256").update(BOT_TOKEN).digest();
+  const checkString = Object.keys(data)
     .sort()
-    .map((key) => `${key}=${rest[key]}`)
+    .map((key) => `${key}=${data[key]}`)
     .join("\n");
 
-  const secret = crypto.createHash("sha256").update(BOT_TOKEN).digest();
-
-  const checkHash = crypto
+  const hmac = crypto
     .createHmac("sha256", secret)
-    .update(sortedData)
+    .update(checkString)
     .digest("hex");
 
-  return hash === checkHash;
-};
-
-router.get("/", (req, res) => {
-  console.log("Request received at /auth/telegram:", req.body);
-  const data = req.body;
-
-  if (!checkTelegramAuth(data)) {
-    return res.status(403).json({ message: "Invalid Telegram data" });
+  if (hmac !== hash) {
+    return res.status(401).json({ error: "Unauthorized" });
   }
 
-  const user = {
-    id: data.id,
-    first_name: data.first_name,
-    last_name: data.last_name,
-    username: data.username,
-    photo_url: data.photo_url,
-  };
-
-  res.send(`
-    <script>
-      window.opener.postMessage(${JSON.stringify(user)}, '${
-    req.headers.origin
-  }');
-      window.close();
-    </script>
-  `);
+  // Пользователь прошел авторизацию
+  res.json({ success: true, user: data });
 });
 
 export default router;
