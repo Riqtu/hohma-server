cecff7562e6fa4babda50e1980f98249b4159a1c
Artem <iriqtu@gmail.com>
2025-01-03 15:27:06 +0300

    add author

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  author: { type: String },
+  authorImg: { type: String },
 });
 
 export default mongoose.model("Movie", movieSchema);
